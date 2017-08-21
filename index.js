6797d4f4e9361cb452095018a1dfb9e4f81ee209
Gang Qiu <gangqiu0@gmail.com>
2017-08-21 16:38:32 -0400

    sets up `game.js` class

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+const Game = require('./src/game');
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -9,10 +11,18 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+
+// game stored in local memory
+let currentGame = null;
+
+app.get('/', (request, response) => {
   response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
+app.post('/move', (request, response) => {
+    console.log(request);
+})
+
+app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'));
 });
---
+++ src/game.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = class Game {
+    constructor() {
+        this.turn = 'x';     // x always starts
+        this.board = {       // 1-3 represents the coordinates of the board
+            '11': null,      // '11' is position (1,1), or the upper left corner
+            '12': null,
+            '13': null,
+            '21': null,
+            '22': null,
+            '23': null,
+            '31': null,
+            '32': null,
+            '33': null,
+        }
+    }
+
+    setSquare (pos) {
+        this.board(pos) = this.turn;                    // set the 
+        this.turn = this.turn === 'x' ? 'o' : 'x';
+    }
+
+    threeInARow (pos1, pos2, pos3) {
+        let rowIsNotNull = this.board[pos1] !== null && this.board[pos2] !== null && this.board[pos3] !== null;
+        let rowIsMatch = this.board[pos1] === this.board[pos2] && this.board[pos1] === this.board[pos3];
+        return rowIsNotNull && rowIsMatch;
+    }
+
+    winnerOfGame () {
+        if (this.threeInARow('11', '12', '13') ||   // horizontal
+            this.threeInARow('21', '22', '23') ||   // horizontal
+            this.threeInARow('31', '32', '33') ||   // horizontal
+            this.threeInARow('11', '21', '31') ||   // vertical
+            this.threeInARow('12', '22', '32') ||   // vertical
+            this.threeInARow('13', '23', '33') ||   // vertical
+            this.threeInARow('11', '22', '33') ||   // diagonal
+            this.threeInARow('13', '22', '31')) {   // diagonal
+            return this.turn;
+        } else {
+            return null;
+        }
+    }
+}
